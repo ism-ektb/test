7e959b630bd8f32247fdfaf23cae49469336301f
ism <ism-ektb@yandex.ru>
2023-03-13 14:04:22 +0500

    1

---
+++ src/Praktikum.java
@@ -0,0 +1,33 @@
+
+    import java.util.Scanner;
+
+    public class Praktikum {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите год:");
+            int year = scanner.nextInt();
+
+            if (isLeapYear(year)) {
+                System.out.println("12.09." + year);
+            }
+            else {
+                System.out.println("13.09." + year);
+            }
+        }
+
+        public static boolean isLeapYear(int year) {
+            if (year % 400 == 0){
+                return true;
+            }
+            else if (year % 4 == 0) {
+                if (year % 100 != 0) {
+                    return true;
+                }
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
